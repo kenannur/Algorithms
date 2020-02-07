fde01f0c6ff46f841416782502ef40db4ddbeec6
Kenan Nur <kenan.nur@livewell.com.tr>
2020-02-07 15:24:12 +0300

    Add - Coderbyte/AlphabetSoup problem

---
+++ Coderbyte/Problems/AlphabetSoup.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace Coderbyte.Problems
+{
+    /*
+    Have the function AlphabetSoup(str) take the str string parameter being passed and 
+    return the string with the letters in alphabetical order(ie.hello becomes ehllo). 
+    
+    Assume numbers and punctuation symbols will not be included in the string.
+    */
+    public static class AlphabetSoup
+    {
+        public static string Solve(string str)
+        {
+            return string.Join("", str.OrderBy(s => s));
+        }
+    }
+}
