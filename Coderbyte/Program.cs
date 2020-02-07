f122fa99ce2ec21db391d505cb88924aa260f310
Kenan Nur <kenan.nur@livewell.com.tr>
2020-02-07 15:27:49 +0300

    Update Program.cs

--- Coderbyte/Program.cs
+++ Coderbyte/Program.cs
@@ -9,6 +9,7 @@ namespace Coderbyte
         {
             _ = FindIntersection.Solve(new string[] { "1, 3, 4, 7, 13", "1, 2, 4, 13, 15" });
             _ = QuestionsMarks.Solve("acc?7??sss?3rr1??????5");
+            _ = AlphabetSoup.Solve("qwerty");
 
             Console.ReadLine();
         }
