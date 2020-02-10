cfaf3c053f88628c94381f2362a17d662273813f
Kenan Nur <kenan.nur@livewell.com.tr>
2020-02-10 16:06:12 +0300

    Add - Coderbyte/LongestWord problem

---
+++ Coderbyte/Problems/LongestWord.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Coderbyte.Problems
+{
+    public static class LongestWord
+    {
+        public static string Solve(string sen)
+        {
+            var words = sen.Split(' ');
+            var longestWord = string.Empty;
+            foreach (var word in words)
+            {
+                if (Regex.IsMatch(word, @"^[a-zA-Z]+$"))
+                {
+                    if (word.Length > longestWord.Length)
+                    {
+                        longestWord = word;
+                    }
+                }
+            }
+            return longestWord;
+        }
+    }
+}
--- Coderbyte/Program.cs
+++ Coderbyte/Program.cs
@@ -10,6 +10,7 @@ namespace Coderbyte
             _ = FindIntersection.Solve(new string[] { "1, 3, 4, 7, 13", "1, 2, 4, 13, 15" });
             _ = QuestionsMarks.Solve("acc?7??sss?3rr1??????5");
             _ = AlphabetSoup.Solve("qwerty");
+            _ = LongestWord.Solve("fun&!! time");
 
             Console.ReadLine();
         }
