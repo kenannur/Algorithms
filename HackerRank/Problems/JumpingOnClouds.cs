8152739d8b613c08ac1748fd158372ec419596cf
Kenan Nur <kenan.nur@livewell.com.tr>
2020-02-24 14:08:57 +0300

    Add - HackerRank/JumpingOnClouds problem

---
+++ HackerRank/Problems/JumpingOnClouds.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace HackerRank.Problems
+{
+    public static class JumpingOnClouds
+    {
+        public static int Solve(int[] c)
+        {
+            var current = 0;
+            var path = new List<int>();
+
+            while(current < c.Length - 1)
+            {
+                var temp = current + 2;
+                if (temp < c.Length)
+                {
+                    if (c[temp] == 0)
+                    {
+                        path.Add(temp);
+                        current += 2;
+                    }
+                    else
+                    {
+                        path.Add(current + 1);
+                        current += 1;
+                    }
+                }
+                else
+                {
+                    path.Add(current + 1);
+                    current += 1;
+                }
+            }
+            return path.Count;
+        }
+    }
+}
--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -10,6 +10,7 @@ namespace HackerRank
             BadNumbers.Solve();
             SequentialNumbers.Solve();
             _ = SockMerchant.Solve(9, new int[] { 10, 20, 20, 10, 10, 30, 50, 10, 20 });
+            _ = JumpingOnClouds.Solve(new int[] { 0, 0, 0, 1, 0, 0 });
             Console.ReadLine();
         }
     }
