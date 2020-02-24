27b3ec081cb4a05de83c52e0ec17913382a3f979
Kenan Nur <kenan.nur@livewell.com.tr>
2020-02-24 14:19:50 +0300

    Add - HackerRank/RepeatedString problem

---
+++ HackerRank/Problems/RepeatedString.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace HackerRank.Problems
+{
+    public static class RepeatedString
+    {
+        public static long Solve(string s, long n)
+        {
+            if (s == "a")
+            {
+                return n;
+            }
+            var aCount = s.Count(x => x == 'a');
+            var exactRepeatCount = n / s.Length;
+            var mod = ((int)(n % s.Length));
+
+            var aCountRemaining = s.Substring(0, mod).Count(x => x == 'a');
+
+            return aCount * exactRepeatCount + aCountRemaining;
+        }
+    }
+}
--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -11,6 +11,7 @@ namespace HackerRank
             SequentialNumbers.Solve();
             _ = SockMerchant.Solve(9, new int[] { 10, 20, 20, 10, 10, 30, 50, 10, 20 });
             _ = JumpingOnClouds.Solve(new int[] { 0, 0, 0, 1, 0, 0 });
+            _ = RepeatedString.Solve("aba", 10);
             Console.ReadLine();
         }
     }
