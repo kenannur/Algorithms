7454d1cc895cfd3aca7cd99131b477a722f69a99
Kenan Nur <kenan.nur@livewell.com.tr>
2020-02-24 15:05:18 +0300

    Add- HackerRank/HourglassSum problem

---
+++ HackerRank/Problems/HourglassSum.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRank.Problems
+{
+    public static class HourglassSum
+    {
+        public static int Solve(int[][] arr)
+        {
+            var sumList = new List<int>();
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    var leftTop = arr[i][j];
+                    var top = arr[i + 1][j];
+                    var rightTop = arr[i + 2][j];
+                    var center = arr[i + 1][j + 1];
+                    var leftBottom = arr[i][j + 2];
+                    var bottom = arr[i + 1][j + 2];
+                    var rightBottom = arr[i + 2][j + 2];
+
+                    var total = leftTop + top + rightTop + center + leftBottom + bottom + rightBottom;
+                    sumList.Add(total);
+                }
+            }
+            var result = sumList.Max();
+            return result;
+        }
+
+        public static int[][] PrepareHourglassSumInput()
+        {
+            var arr = new int[6][];
+            arr[0] = new int[6];
+            arr[1] = new int[6];
+            arr[2] = new int[6];
+            arr[3] = new int[6];
+            arr[4] = new int[6];
+            arr[5] = new int[6];
+
+            arr[0][0] = 1;
+            arr[1][0] = 1;
+            arr[2][0] = 1;
+            arr[3][0] = 0;
+            arr[4][0] = 0;
+            arr[5][0] = 0;
+
+            arr[0][1] = 0;
+            arr[1][1] = 1;
+            arr[2][1] = 0;
+            arr[3][1] = 0;
+            arr[4][1] = 0;
+            arr[5][1] = 0;
+
+            arr[0][2] = 1;
+            arr[1][2] = 1;
+            arr[2][2] = 1;
+            arr[3][2] = 0;
+            arr[4][2] = 0;
+            arr[5][2] = 0;
+
+            arr[0][3] = 0;
+            arr[1][3] = 0;
+            arr[2][3] = 2;
+            arr[3][3] = 4;
+            arr[4][3] = 4;
+            arr[5][3] = 0;
+
+            arr[0][4] = 0;
+            arr[1][4] = 0;
+            arr[2][4] = 0;
+            arr[3][4] = 2;
+            arr[4][4] = 0;
+            arr[5][4] = 0;
+
+            arr[0][5] = 0;
+            arr[1][5] = 0;
+            arr[2][5] = 1;
+            arr[3][5] = 2;
+            arr[4][5] = 4;
+            arr[5][5] = 0;
+            return arr;
+        }
+    }
+}
--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -12,6 +12,9 @@ namespace HackerRank
             _ = SockMerchant.Solve(9, new int[] { 10, 20, 20, 10, 10, 30, 50, 10, 20 });
             _ = JumpingOnClouds.Solve(new int[] { 0, 0, 0, 1, 0, 0 });
             _ = RepeatedString.Solve("aba", 10);
+            _ = HourglassSum.Solve(HourglassSum.PrepareHourglassSumInput());
+
+
             Console.ReadLine();
         }
     }
