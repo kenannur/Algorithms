db33efcb66b14d5a8863c18a5e627033006cd1a1
Kenan Nur <kenan.nur@livewell.com.tr>
2020-02-24 13:44:03 +0300

    Add - HackerRank/SockMerchant problem

---
+++ HackerRank/Problems/SockMerchant.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace HackerRank.Problems
+{
+    public static class SockMerchant
+    {
+        public static int Solve(int n, int[] arr)
+        {
+            var result = 0;
+            var groupedArray = arr.GroupBy(x => x).Where(g => g.Count() >= 2);
+            foreach (var group in groupedArray)
+            {
+                var pairCount = group.Count() / 2;
+                result += pairCount;
+            }
+            return result;
+        }
+    }
+}
--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -9,6 +9,7 @@ namespace HackerRank
         {
             BadNumbers.Solve();
             SequentialNumbers.Solve();
+            _ = SockMerchant.Solve(9, new int[] { 10, 20, 20, 10, 10, 30, 50, 10, 20 });
             Console.ReadLine();
         }
     }
