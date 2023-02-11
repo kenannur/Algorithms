231e5e2943e2e61cbe6f70455da52de710ef3bc2
kenannur <kenannur@hotmail.com>
2023-02-11 23:01:13 +0300

    Threecolts algorithms added

---
+++ Other/Problems/Threecolts.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Other.Problems;
+
+public static class Threecolts
+{
+    public static int CountUniqueUrls(string[] urls)
+    {
+        return urls
+            .Where(x => !string.IsNullOrWhiteSpace(x) && Uri.IsWellFormedUriString(x, UriKind.Absolute))
+            .Select(x => new Uri(x.ToLower()))
+            .GroupBy(x => new
+            {
+                x.Scheme,
+                Host = x.Host.Replace("www.", ""),
+                AbsolutePath = x.AbsolutePath.Replace("//", "/")
+            }).Count();
+    }
+
+
+    public static Dictionary<string, int> CountUniqueUrlsPerTopLevelDomain(string[] urls)
+    {
+        return urls
+            .Where(x => !string.IsNullOrWhiteSpace(x) && Uri.IsWellFormedUriString(x, UriKind.Absolute))
+            .Select(x => new Uri(x.ToLower()))
+            .GroupBy(x =>
+            {
+                var normalizedHost = x.Host.Replace("www.", "");
+                var topLevelDomainParts = normalizedHost.Split('.').TakeLast(2);
+                var topLevelDomain = string.Join('.', topLevelDomainParts);
+                return topLevelDomain;
+            }).ToDictionary(x => x.Key, y => y.Count());
+    }
+}
+
--- Other/Program.cs
+++ Other/Program.cs
@@ -8,6 +8,21 @@ namespace Other
         static void Main(string[] args)
         {
             WeightDivider.Solve();
+
+            var r1 = Threecolts.CountUniqueUrls(new[] { "https://example.com", "https://example.com/" });
+            var r2 = Threecolts.CountUniqueUrls(new[] { "https://example.com", "http://example.com" });
+            var r3 = Threecolts.CountUniqueUrls(new[] { "https://example.com?", "https://example.com" });
+            var r4 = Threecolts.CountUniqueUrls(new[] { "https://example.com?a=1&b=2", "https://example.com?b=2&a=1" });
+            var r5 = Threecolts.CountUniqueUrls(new[] { "http://example.com/foo%2a", "http://example.com/foo%2A" });
+            var r6 = Threecolts.CountUniqueUrls(new[] { "HTTP://User@Example.COM/Foo", "http://User@example.com/Foo" });
+            var r7 = Threecolts.CountUniqueUrls(new[] { "http://example.com/%7Efoo", "http://example.com/~foo" });
+            var r8 = Threecolts.CountUniqueUrls(new[] { "http://example.com/foo/./bar/baz/../qux", "http://example.com/foo/bar/qux" });
+            var r9 = Threecolts.CountUniqueUrls(new[] { "http://www.example.com/", "http://example.com/" });
+            var r10 = Threecolts.CountUniqueUrls(new[] { "http://example.com/foo//bar.html", "http://example.com/foo/bar.html" });
+
+            var t1 = Threecolts.CountUniqueUrlsPerTopLevelDomain(new[] { "https://example.com", "https://subdomain.example.com" });
+            var t2 = Threecolts.CountUniqueUrlsPerTopLevelDomain(new[] { "https://test.example.com", "https://test.test.com" });
+
             Console.ReadLine();
         }
     }
